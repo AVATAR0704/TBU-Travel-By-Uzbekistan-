b5be8c05108c860d2199bd26062678f2da03bad1
Samandar <samandartoshpulatov0704@gmail.com>
2024-05-20 13:44:46 +0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,22 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Macservis on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    
+    @StateObject private var vm: LocationsViewModel = LocationsViewModel()
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            LocationView()
+                .environmentObject(vm)
+        }
+    }
+}
